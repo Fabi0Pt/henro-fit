25023f6adb61975ae8abfbea4b4a36be6f3df1bf
Fábio Henro <fabi0_pt@hotmail.com>
2022-12-11 18:02:04 -0300

    feat: add light theme

---
+++ src/shared/themes/Light.ts
@@ -0,0 +1,22 @@
+import { createTheme } from "@mui/material";
+
+export const LightTheme = createTheme({
+  palette: {
+    primary: {
+      main: "#e02222",
+      light: "#a82727",
+      dark: "#941515",
+      contrastText: "#E1E1E6",
+    },
+    secondary: {
+      main: "#e02222",
+      light: "#a82727",
+      dark: "#941515",
+      contrastText: "#E1E1E6",
+    },
+    background: {
+      paper: "#e8e8eb",
+      default: "#E1E1E6",
+    },
+  },
+});
